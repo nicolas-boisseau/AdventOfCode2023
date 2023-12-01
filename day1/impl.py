5c10c36f071383ef99e27a9a9d8f4875e7b90153
nicolas.boisseau <nicolas.boisseau@outlook.com>
2023-12-01 07:49:37 +0100

    day1

--- day1/impl.py
+++ day1/impl.py
@@ -1,5 +1,8 @@
 import itertools
 import os.path
+
+from py_linq import Enumerable
+
 #from py_linq import Enumerable
 
 from common.common import DownloadIfNotExists, DetectCurrentDay, PostAnswer
@@ -16,22 +19,58 @@ def process(part, filename):
         print("Input file not found !")
 
     print("Input file OK ! Starting processing...")
+
+    total = 0
     with open(filename) as f:
         lines = [l.replace("\n", "") for l in f.readlines()]
 
-        # TODO
+        for line in lines:
 
-    return 0
+            if part == 2:
+                line = replaceNumbers(line)
+
+            letters = Enumerable([l for l in line])
+            digits = letters.where(lambda l: l.isdigit())
+            #print(digits)
+
+            number = int(digits.to_list()[0] + digits.to_list()[-1])
+            #print(number)
+            total += number
+
+
+    return total
+
+def replaceNumbers(line):
+    newLineSoFar = ""
+    for l in line:
+        newLineSoFar += l
+        newLineSoFar = replaceNumberRaw(newLineSoFar)
+    return newLineSoFar
+
+def replaceNumberRaw(line):
+    line = line.replace("one", "1")
+    line = line.replace("two", "2")
+    line = line.replace("three", "3")
+    line = line.replace("four", "4")
+    line = line.replace("five", "5")
+    line = line.replace("six", "6")
+    line = line.replace("seven", "7")
+    line = line.replace("eight", "8")
+    line = line.replace("nine", "9")
+    # line = line.replace("zero", "0")
+    return line
 
 if __name__ == '__main__':
 
-    level = 1
-    expectedSampleResult = 7
+    level = 2
+    expectedSampleResult = 281
+    sampleFile = "sample2.txt"
 
-    if process(level, "sample.txt") == expectedSampleResult:
-        print("Part 1 sample OK")
+    if process(level, sampleFile) == expectedSampleResult:
+        print(f"Part {level} sample OK")
 
         result = process(level, "input.txt")
+        print(f"Part {level} result is {result}")
 
         day = DetectCurrentDay()
         if day != 0:
--- day1/tests.py
+++ day1/tests.py
@@ -5,16 +5,16 @@ from impl import process
 class AdventOfCodeTests(unittest.TestCase):
 
     def test_part1_sample(self):
-        self.assertEqual(process(1, "sample.txt"), 7)
+        self.assertEqual(process(1, "sample.txt"), 142)
 
     def test_part1_input(self):
-        self.assertEqual(process(1, "input.txt"), 1804)
+        self.assertEqual(process(1, "input.txt"), 54561)
 
     def test_part2_sample(self):
-        self.assertEqual(process(2, "sample.txt"), 19)
+        self.assertEqual(process(2, "sample2.txt"), 281)
 
     def test_part2_input(self):
-        self.assertEqual(process(2, "input.txt"), 2508)
+        self.assertEqual(process(2, "input.txt"), 54076)
 
 
 if __name__ == '__main__':
