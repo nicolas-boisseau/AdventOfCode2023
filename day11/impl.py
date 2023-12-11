55f0df2fbf338df410e0a32cea814b3b71636bf9
nicolas.boisseau <nicolas.boisseau@outlook.com>
2023-12-11 13:49:33 +0100

    day 11 part 2 (astar algorithm was finally useless...)

--- day11/impl.py
+++ day11/impl.py
@@ -19,30 +19,31 @@ def findGalaxiesPositions(lines):
     return galaxies
 
 
-def expand_rows(lines):
+def get_rows_to_expand(lines):
     to_insert = []
     for r, line in enumerate(lines):
         if Enumerable(line).all(lambda x: x == "."):
             to_insert.append(r)
 
-    to_insert.reverse()
-    for r in to_insert:
-        lines.insert(r, "." * len(lines[0]))
+    # to_insert.reverse()
+    # for r in to_insert:
+    #     lines.insert(r, "." * len(lines[0]))
+    #
+    # return lines
+    return to_insert
 
-    return lines
-
-def expand_columns(lines):
+def get_cols_to_expand(lines):
     to_insert = []
     for c, _ in enumerate(lines[0]):
         if (Enumerable([line[c] for line in lines]).all(lambda x: x == ".")):
             to_insert.append(c)
 
-    to_insert.reverse()
-    for r_to_insert in to_insert:
-        for i, line in enumerate(lines):
-            lines[i] = line[:r_to_insert] + "." + line[r_to_insert:]
+    # to_insert.reverse()
+    # for r_to_insert in to_insert:
+    #     for i, line in enumerate(lines):
+    #         lines[i] = line[:r_to_insert] + "." + line[r_to_insert:]
 
-    return lines
+    return to_insert
 
 
 def print_lines(lines):
@@ -50,7 +51,7 @@ def print_lines(lines):
         print(line)
 
 
-def process(part, filename):
+def process(part, filename, expand=1):
     if not (os.path.exists(filename)):
         print("Input file not found !")
 
@@ -59,8 +60,13 @@ def process(part, filename):
     with open(filename) as f:
         lines = [line.replace("\n", "") for line in f.readlines()]
 
-        lines = expand_rows(lines)
-        lines = expand_columns(lines)
+        rows_to_expand = get_rows_to_expand(lines)
+        cols_to_expand = get_cols_to_expand(lines)
+
+        to_expand = 1
+        if part == 2:
+            to_expand = expand
+
         #print_lines(lines)
         galaxies = findGalaxiesPositions(lines)
         print(f"nb galaxies: {len(galaxies)}")
@@ -88,22 +94,39 @@ def process(part, filename):
                 other_galaxy = galaxies[j_g]
                 if i_g != j_g and ((galaxy[0], other_galaxy[0]) not in pairs) and ((other_galaxy[0], galaxy[0]) not in pairs):
 
-                    if galaxy[1][0] == other_galaxy[1][0]:
-                        pairs[(galaxy[0], other_galaxy[0])] = abs(galaxy[1][1] - other_galaxy[1][1])
-                    elif galaxy[1][1] == other_galaxy[1][1]:
-                        pairs[(galaxy[0], other_galaxy[0])] = abs(galaxy[1][0] - other_galaxy[1][0])
-                    else:
+                    x1 = galaxy[1][0]
+                    y1 = galaxy[1][1]
+                    x2 = other_galaxy[1][0]
+                    y2 = other_galaxy[1][1]
 
-                        x1 = galaxy[1][0]
-                        y1 = galaxy[1][1]
-                        x2 = other_galaxy[1][0]
-                        y2 = other_galaxy[1][1]
-                        pairs[(galaxy[0], other_galaxy[0])] = abs(x2 - x1) + abs(y2 - y1)
+                    toadd_y2 = toadd_y1 = 0
+                    for expand_row in rows_to_expand:
+                        if y1 < y2 and y1 < expand_row < y2:
+                            toadd_y2 += (to_expand-1 if to_expand > 1 else 1)
+                        elif y1 > y2 and y2 < expand_row < y1:
+                            toadd_y1 += (to_expand-1 if to_expand > 1 else 1)
+                    y2 += toadd_y2
+                    y1 += toadd_y1
+
+                    toadd_x2 = toadd_x1 = 0
+                    for expand_col in cols_to_expand:
+                        if x1 < x2 and x1 < expand_col < x2:
+                            toadd_x2 += (to_expand-1 if to_expand > 1 else 1)
+                        elif x1 > x2 and x2 < expand_col < x1:
+                            toadd_x1 += (to_expand-1 if to_expand > 1 else 1)
+                    x2 += toadd_x2
+                    x1 += toadd_x1
+
+                    pairs[(galaxy[0], other_galaxy[0])] = abs(x2 - x1) + abs(y2 - y1)
+
+
+
+
                     # else:
                     #     path = astar.astar(f"{galaxy[1][0]},{galaxy[1][1]}", f"{other_galaxy[1][0]},{other_galaxy[1][1]}")
                     #     path = list(path)
                     #     pairs[(galaxy[0], other_galaxy[0])] = len(path)-1
-                print(f"Computed galaxies: {len(pairs)}")
+                #print(f"Computed galaxies: {len(pairs)}")
 
     return sum(pairs.values())
 
--- day11/tests.py
+++ day11/tests.py
@@ -8,13 +8,14 @@ class AdventOfCodeTests(unittest.TestCase):
         self.assertEqual(process(1, "sample.txt"), 374)
 
     def test_part1_input(self):
-        self.assertEqual(process(1, "input.txt"), 1804)
+        self.assertEqual(process(1, "input.txt"), 9177603)
 
     def test_part2_sample(self):
-        self.assertEqual(process(2, "sample.txt"), 19)
+        self.assertEqual(1030, process(2, "sample.txt", 10))
+        self.assertEqual(8410, process(2, "sample.txt", 100))
 
     def test_part2_input(self):
-        self.assertEqual(process(2, "input.txt"), 2508)
+        self.assertEqual(632003913611, process(2, "input.txt", 1000000))
 
 
 if __name__ == '__main__':
